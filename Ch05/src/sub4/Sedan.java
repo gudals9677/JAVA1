6373ed73cecf3588d7ee19f584b73986436e0753
gudals9677 <gudals9677@gmail.com>
2024-01-03 12:03:12 +0900

    상속 다형성

---
+++ Ch05/src/sub4/Sedan.java
@@ -0,0 +1,23 @@
+package sub4;
+
+public class Sedan extends Car {
+	
+	private int cc;
+	
+	
+	public Sedan(String name, String color, int speed,int cc) { //Car의 생성자 그대로 가져옴
+		super(name,color,speed); // 부모클래스 생성자 호출
+		this.cc = cc;
+	}
+	
+	public void speedTurbo() {
+		this.speed += 20;
+	}
+	
+	public void show() {
+		super.show();
+		System.out.println("배기량: " + this.cc);
+	}
+	
+	
+}
