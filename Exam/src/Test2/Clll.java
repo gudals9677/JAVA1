b4b974b7b048ec2b0dfbb0431c144920087863ce
gudals9677 <gudals9677@gmail.com>
2024-01-26 17:49:54 +0900

    123

---
+++ Exam/src/Test2/Clll.java
@@ -0,0 +1,16 @@
+package Test2;
+
+public class Clll {
+	public static void main(String[] args) {
+		
+		int result =0;
+		
+		for(int k=1; k<=10; k++) {
+			if(k%2==1) {
+				result +=k;
+			}
+			
+		}
+		System.out.println(result);
+	}
+}
